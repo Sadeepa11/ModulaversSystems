e78e82ce07292adf0092d8a6c278673136956457
Sadeepa11 <srisadeepa658@gmail.com>
2025-08-08 15:59:44 +0530

    Fix typo in import path (componets ➝ components)

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,18 @@
 import Welcome from "./componets/Welcome";
-import ServicesGrid from "./componets/services";
 import TeamSection from "./componets/TeamSection";
 import ModulaversAboutSection from "./componets/About";
 import ContactDetectionForm from "./componets/Contact";
 import SoftwareCompanyFooter from "./componets/Footer";
+import ServicesSection from "./componets/Services";
 
 export default function Home() {
   return (
   <div className="w-[100] items-center justify-items-center min-h-screen ">
   <Welcome />
-  <ServicesGrid/>
-  <TeamSection/>
+  {/* <ServicesGrid/> */}
+
+  <ServicesSection/>
+    <TeamSection/>
   <ModulaversAboutSection/>
   <ContactDetectionForm/>
   <SoftwareCompanyFooter/>
