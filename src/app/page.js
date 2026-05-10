fa516275592538103b31c821dc30ed2229aa1ce8
Sadeepa11 <srisadeepa658@gmail.com>
2026-05-10 23:42:47 +0530

    feat: implement home page structure with imported landing page sections

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ import ServicesSection from "./componets/Services";
 
 export default function Home() {
   return (
-  <div className="w-[100] items-center justify-items-center min-h-screen ">
+  <div className="w-full min-h-screen">
   <Welcome />
   {/* <ServicesGrid/> */}
 
